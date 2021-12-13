23a35191ee80be1e6d0ca587a2600dfddc3ba9ef
skyye59 <azureskyye59@gmail.com>
2021-12-13 08:44:43 -0400

    Added files to partial for editing, updated index file

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 import express from "express";
 import path from "path";
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const __dirname = path.resolve();
 const app = express();
+
+// Connect to SimfoniDB
+
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://admin:admin@simfonicluster.91e1d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 // Use Static Public Folder
 app.use(express.static("public"));
---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+  organization: {
+    type: String,
+    require: false,
+  },
+  phoneNumber: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
