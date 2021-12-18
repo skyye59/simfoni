dea913343e1b6ba59eeebf92a6398a0c1f273395
skyye59 <azureskyye59@gmail.com>
2021-12-18 18:09:49 -0400

    overhaul of the backend

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
-import express from "express";
-import path from "path";
+const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 
-const __dirname = path.resolve();
+// Create the app
 const app = express();
 
-// Connect to SimfoniDB
-
-const { MongoClient } = require("mongodb");
-const uri =
-  "mongodb+srv://admin:admin@simfonicluster.91e1d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
-// Use Static Public Folder
+// Middleware
 app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
 
+// Set the view engine for the app
 app.set("view engine", "ejs");
 
-app.listen(3000);
+// Database connection
+const dbURI =
+  "mongodb+srv://admin:admin@simfonicluster.91e1d.mongodb.net/simfoniDB?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  //   res.send("<h1>Hello, world</h1>");
-  res.render("index");
-});
-
-app.get("/home", (req, res) => {
-  //   res.send("<h1>About, world</h1>");
-  res.render("home", { root: __dirname });
-});
+// routes
+app.get("*", checkUser);
+app.get("/", (req, res) => res.render("index"));
+app.get("/home", requireAuth, (req, res) => res.render("home"));
+app.use(authRoutes);
